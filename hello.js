704e136eb07b2f0bd6e7ad6fb5d5cbd70828b9f0
dansy-rodz <dansyrodrigues@gmail.com>
2025-06-05 10:37:42 +0530

    i learned func

--- hello.js
+++ hello.js
@@ -1,9 +1,11 @@
 //const prompt = require('prompt-sync')();
 //const name = prompt("What is your name? ");
 //console.log("Hello, " + name + "!");
+f
+//function example
 function sq(x){
     let ans = 0;
     ans = x*x;
     return ans;
 }
-console.log("anwser"+ sq(2));
+console.log("anwser:"+ sq(2));
