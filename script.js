273314df2cc451ead508e35797fe50381d8ff9f7
dansy-rodz <dansyrodrigues@gmail.com>
2025-06-05 09:27:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello world!");
+let val ="hello";
+val=false;
+console.log(val)
+const pie=3.14;
+console.log(pie)
+let num = 22
+if (num%2 == 0 ){
+    console.log("no. is even")
+}else{
+    console.log("no. is odd")
+}
